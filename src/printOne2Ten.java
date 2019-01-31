78d569173e81496cee51db518f782aa5b9b69a01
sw333zy@gmail.com <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-01-31 11:43:03 -0500

    Initial commit

---
+++ src/printOne2Ten.java
@@ -0,0 +1,8 @@
+public class printOne2Ten {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
